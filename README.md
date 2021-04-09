ae34808a800f291b5336dba28aefb86963b2fc8f
Willian <williamhernandez450@gmail.com>
2021-04-08 20:44:02 -0600

    agregando proyecto

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Random_Image" 
+"# Random_Image" 
---
+++ app/src/main/java/com/willianhdz/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.willianhdz.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+//    Inicializa la variable fuera del constructor
+//    esto mejora el rendimiento de la app y genera ahoorro de recursos en el movil
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    val randomInt = (1..6).random()
+
+    private fun rollDice() {
+//        Toast.makeText(this, "Boton clickeado",
+//            Toast.LENGTH_SHORT).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+}
